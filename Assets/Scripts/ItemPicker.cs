040af2ccce78e5530d6866955bc9d7868bd03485
Akure <alexwaisley@gmail.com>
2022-01-22 07:11:13 +0200

    1212

--- Assets/Scripts/ItemPicker.cs
+++ Assets/Scripts/ItemPicker.cs
@@ -9,14 +9,15 @@ public class ItemPicker : MonoBehaviour
 {
     [SerializeField] public List<string> items;
     [SerializeField] private Text itemCounter;
+    [SerializeField] private Text itemCounterOut;
 
     private void OnTriggerEnter2D(Collider2D other)
-    { 
-
+    {
         if(!other.gameObject.CompareTag("Item")) return;
         var item = other.gameObject.GetComponent<ItemObject>();
         items.Add(item.id);
         itemCounter.text = items.Count.ToString();
+        itemCounterOut.text = items.Count.ToString()+"/6";
         PlayerPrefs.SetInt($"Frags{SceneManager.GetActiveScene().name}", items.Count);
         PlayerPrefs.Save();
         Destroy(other.gameObject);
