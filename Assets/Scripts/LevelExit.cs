5141ddf11a1762d914652d7c9e527a139e98c3d0
Akure <alexwaisley@gmail.com>
2022-01-22 22:44:58 +0200

    8800

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -3,17 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
 
 public class LevelExit : MonoBehaviour
 {
     [SerializeField] private UnityEvent onExit;
+    [SerializeField] private GameObject exitLogS;
+    [SerializeField] private Movement movement;
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log(onExit);
         onExit.Invoke();
         Debug.Log(other.gameObject.tag);
     }
-    
-
+    public void HideExitPanelS()
+    {
+        exitLogS.SetActive(false);
+        movement.freeze = false;
+    }
+    public void ProfitS()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    private void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.P)) return;
+        Debug.Log(exitLogS);
+        Debug.Log(exitLogS.gameObject);
+        exitLogS.SetActive(true);
+        movement.freeze = true;
+    }
 }
--- Assets/Scripts/UiControl.cs
+++ Assets/Scripts/UiControl.cs
@@ -5,19 +5,20 @@ using UnityEngine.SceneManagement;
 public class UiControl : MonoBehaviour
 {
     [SerializeField] private GameObject exitLog;
-
+    
     [SerializeField] private Movement movement;
     public void ShowExitPanel()
     {
         exitLog.SetActive(true);
         movement.freeze = true;
     }
-
+    
     public void HideExitPanel()
     {
         exitLog.SetActive(false);
         movement.freeze = false;
     }
+    
 
     public void Profit()
     {
