a7b2f6a1be242a023f45db7d03fc5562cdf1ce1e
Akure <75520842+AlexWaisley@users.noreply.github.com>
2022-04-14 15:56:29 +0300

    ere

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,16 +9,64 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private GameObject movementPanel;
     [SerializeField] private GameObject mainPanel;
     [SerializeField] private GameObject levelPanel;
+    [SerializeField] private GameObject virusInstr;
     [SerializeField] private GameObject attentionPanel;
     [SerializeField] private GameObject aboutPanel;
+    [SerializeField] private GameObject instrPanel;
     [SerializeField] private GameObject confirmExitPanel;
     [SerializeField] private List<UnityEngine.UI.Image> levelButtons;
     [SerializeField] private List<UnityEngine.UI.Text> fragCountOnLvl;
+    [SerializeField] private List<GameObject> info1;
+    [SerializeField] private List<GameObject> instr1;
+    [SerializeField] private List<GameObject> info2;
+    [SerializeField] private List<GameObject> instr2;
+    [SerializeField] private List<GameObject> info3;
+    [SerializeField] private List<GameObject> instr3;
     [SerializeField] private Color c1;
     [SerializeField] private Color c2;
     [SerializeField] private Color c3;
 
     public void LoadLevel(int level) => SceneManager.LoadScene($"Lvl_{level}");
+
+    public void ShowInstr_1()
+    {
+        var acLst = MemoryManager.GetValues();
+        virusInstr.SetActive(true);
+        for (var i = 0; i < acLst[1]; i++)
+        {
+            info1[i].SetActive(true);
+        }
+        for (var i = 0; i < acLst[2]; i++)
+        {
+            instr1[i].SetActive(true);
+        }
+    }
+    public void ShowInstr_2()
+    {
+        var acLst = MemoryManager.GetValues();
+        virusInstr.SetActive(true);
+        for (var i = 0; i < acLst[3]; i++)
+        {
+            info2[i].SetActive(true);
+        }
+        for (var i = 0; i < acLst[4]; i++)
+        {
+            instr2[i].SetActive(true);
+        }
+    }
+    public void ShowInstr_3()
+    {
+        var acLst = MemoryManager.GetValues();
+        virusInstr.SetActive(true);
+        for (var i = 0; i < acLst[5]; i++)
+        {
+            info3[i].SetActive(true);
+        }
+        for (var i = 0; i < acLst[6]; i++)
+        {
+            instr3[i].SetActive(true);
+        }
+    }
     public void ResetData()
     {
         MemoryManager.ResetData();
@@ -47,8 +95,10 @@ public class MainMenu : MonoBehaviour
         mainPanel.SetActive(false);
         levelPanel.SetActive(false);
         attentionPanel.SetActive(false);
+        virusInstr.SetActive(false);
         aboutPanel.SetActive(false);
         confirmExitPanel.SetActive(false);
+        instrPanel.SetActive(false);
         panel.SetActive(true);
     }
     public void Exit() => Application.Quit();
